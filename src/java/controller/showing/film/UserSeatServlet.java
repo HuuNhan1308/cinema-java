0174a0598843fbf88661401c0eab67073ce0a258
hhnhan1308 <nhanhohuunhan7398@gmail.com>
2023-11-22 00:29:12 +0700

    modify seat servlet

--- src/java/controller/showing/film/UserSeatServlet.java
+++ src/java/controller/showing/film/UserSeatServlet.java
@@ -32,144 +32,129 @@ import javax.servlet.http.HttpSession;
  *
  * @author Admin
  */
-@WebServlet(name = "UserSeatServlet", urlPatterns = { "/showing/film/seat" })
+@WebServlet(name = "UserSeatServlet", urlPatterns = {"/showing/film/seat"})
 public class UserSeatServlet extends HttpServlet {
 
-    protected void bookingSeats(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String url = "/seat.jsp";
+  protected void bookingSeats(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String url = "/seat.jsp";
 
-        // Get seats
-        String[] seatNumbersArray = request.getParameter("seatNumbers").split(",");
-        int[] seatNumbersInt = Arrays.stream(seatNumbersArray).mapToInt(Integer::parseInt).toArray();
+    // Get seats
+    String[] seatNumbersArray = request.getParameter("seatNumbers").split(",");
+    int[] seatNumbersInt = Arrays.stream(seatNumbersArray).mapToInt(Integer::parseInt).toArray();
 
-        // Get customer
-        HttpSession session = request.getSession();
-        Customer customer = (Customer) session.getAttribute("customer");
-        if (customer == null) {
-            response.sendRedirect(request.getContextPath() + "/login");
-            return;
-        }
+    // Get customer
+    HttpSession session = request.getSession();
+    Customer customer = (Customer) session.getAttribute("customer");
+    if (customer == null) {
+      response.sendRedirect(request.getContextPath() + "/login");
+      return;
+    }
 
-        // Get showtime
-        String showtimeId = request.getParameter("showtimeId");
-        ShowTime showtime = ShowTimeDB.selectShowTime(showtimeId);
+    // Get showtime
+    String showtimeId = request.getParameter("showtimeId");
+    ShowTime showtime = ShowTimeDB.selectShowTime(showtimeId);
 
-        // Create invoice
-        Invoice invoice = new Invoice();
-        invoice.setCustomer(customer);
-        invoice.setBoughDate(java.sql.Date.valueOf(java.time.LocalDate.now()));
+    // Create invoice
+    Invoice invoice = new Invoice();
+    invoice.setCustomer(customer);
+    invoice.setBoughDate(java.sql.Date.valueOf(java.time.LocalDate.now()));
+    invoice.setTickets(new ArrayList<Ticket>());
+    
+    // Create ticket and add to the new invoice
+    for (int seatNumber : seatNumbersInt) {
+      Ticket ticket = new Ticket();
+      ticket.setInvoice(invoice);
+      ticket.setSeatNumber(seatNumber);
+      ticket.setShowtime(showtime);
 
-        for (int seatNumber : seatNumbersInt) {
-            Ticket ticket = new Ticket();
-            ticket.setInvoice(invoice);
-            ticket.setSeatNumber(seatNumber);
-            ticket.setShowtime(showtime);
+      // Determine seat class based on seat number
+      int seatClassId = (seatNumber >= 31 && seatNumber <= 60) ? 3 : 2;
+      SeatClass seatClass = SeatClassDB.selectSeatClass(seatClassId);
+      ticket.setSeatClass(seatClass);
 
-            // Determine seat class based on seat number
-            int seatClassId = (seatNumber >= 31 && seatNumber <= 60) ? 3 : 2;
-            SeatClass seatClass = SeatClassDB.selectSeatClass(seatClassId);
-            ticket.setSeatClass(seatClass);
+      invoice.getTickets().add(ticket);
+    }
 
-            invoice.getTickets().add(ticket);
-        }
+    List<Ticket> tickets = invoice.getTickets();
+    
+    // Insert tickets
+    // Update the invoice with the tickets
+    // Calculate total cost
+    double total = 0;
+    for (Ticket ticket : tickets) {
+      SeatClass seatClass = ShowTimeSeatDB.getSeatClass(ticket.getSeatClass().getId());
+      total += ticket.getShowtime().getPrice() * seatClass.getFactor();
+    }
 
-        // Persist the invoice first
-        InvoiceDB.insert(invoice);
-
-        // // Create tickets
-        // List<Ticket> tickets = new ArrayList<>();
-        // for (int seatNumber : seatNumbersInt) {
-        // String ticketId = UUID.randomUUID().toString();
-        // Ticket ticket = new Ticket();
-        // ticket.setTicketID(ticketId);
-        // ticket.setSeatNumber(seatNumber);
-        // ticket.setInvoice(invoice);
-        // ticket.setShowtime(ShowTimeDB.selectShowTime(showtimeId));
-        //
-        // // Determine seat class based on seat number
-        // int seatClassId = (seatNumber >= 31 && seatNumber <= 60) ? 3 : 2;
-        // SeatClass seatClass = SeatClassDB.selectSeatClass(seatClassId);
-        // ticket.setSeatClass(seatClass);
-        //
-        // tickets.add(ticket);
-        // }
-
-        List<Ticket> tickets = invoice.getTickets();
-
-        // Insert tickets
-        // Update the invoice with the tickets
-        // Calculate total cost
-        double total = 0;
+    // Check if customer has enough balance
+    try {
+      // Check if customer has enough balance
+      if (customer.getBalance() >= total) {
+        double newBalance = customer.getBalance() - total;
+        customer.setBalance(newBalance);
+        CustomerDB.update(customer);
+        
         for (Ticket ticket : tickets) {
-            SeatClass seatClass = ShowTimeSeatDB.getSeatClass(ticket.getSeatClass().getId());
-            total += ticket.getShowtime().getPrice() * seatClass.getFactor();
+          TicketDB.insert(ticket);
         }
+        
+        invoice.setTickets(tickets);
+        InvoiceDB.insert(invoice);
+        session.setAttribute("state", "book_success");
+      } else {
+        tickets = null; //remove tickets
+        invoice = null; //remove invoice
+        session.setAttribute("state", "book_fail");
+      }
+    } catch (Exception e) {
+      // Log the exception
+      e.printStackTrace();
 
-        // Check if customer has enough balance
-        try {
-            // Check if customer has enough balance
-            if (customer.getBalance() >= total) {
-                double newBalance = customer.getBalance() - total;
-                CustomerDB.updateBalance(customer, newBalance);
-                for (Ticket ticket : tickets) {
-                    TicketDB.insert(ticket);
-                }
-                invoice.setTickets(tickets);
-                InvoiceDB.update(invoice);
-                session.setAttribute("state", "book_success");
-            } else {
-                tickets.clear();
-                InvoiceDB.delete(invoice);
-                session.setAttribute("state", "book_fail");
-            }
-        } catch (Exception e) {
-            // Log the exception
-            e.printStackTrace();
-
-            // Redirect to home page
-            response.sendRedirect(request.getHeader("Referer"));
-            session.setAttribute("state", "book_fail");
-        }
-
-        // Forward to the same page
-        response.sendRedirect(request.getHeader("Referer"));
-
+      // Redirect to home page
+      response.sendRedirect(request.getHeader("Referer"));
+      session.setAttribute("state", "book_fail");
     }
 
-    @Override
-    @SuppressWarnings("empty-statement")
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String url = "/seat.jsp";
-        String showtimeId = request.getParameter("showtimeId");
+    // Forward to the same page
+    response.sendRedirect(request.getHeader("Referer"));
 
-        if (showtimeId != null && !showtimeId.isEmpty()) {
-            ShowTime showtime = ShowTimeDB.selectShowTime(showtimeId);
-            List<Ticket> chosenSeats = showtime.getTickets();
+  }
 
-            if (showtime != null) {
-                request.setAttribute("showtime", showtime);
-                request.setAttribute("chosenSeats", chosenSeats);
+  @Override
+  @SuppressWarnings("empty-statement")
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String url = "/seat.jsp";
+    String showtimeId = request.getParameter("showtimeId");
 
-                System.out.println(chosenSeats.get(3).getSeatNumber());
-                request.getRequestDispatcher(url).forward(request, response);
-            } else {
-                System.out.println("Error retrieving chosenSeats or showtime from database.");
-            }
-        } else {
-            System.out.println("showtimeId is not set correctly.");
-        }
+    if (showtimeId != null && !showtimeId.isEmpty()) {
+      ShowTime showtime = ShowTimeDB.selectShowTime(showtimeId);
+      List<Ticket> chosenSeats = showtime.getTickets();
+
+      if (showtime != null) {
+        request.setAttribute("showtime", showtime);
+        request.setAttribute("chosenSeats", chosenSeats);
+
+        System.out.println(showtime);
+        System.out.println(chosenSeats);
+        request.getRequestDispatcher(url).forward(request, response);
+      } else {
+        System.out.println("Error retrieving chosenSeats or showtime from database.");
+      }
+    } else {
+      System.out.println("showtimeId is not set correctly.");
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String action = request.getParameter("action");
+    if ("bookingSeats".equals(action)) {
+      this.bookingSeats(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if ("bookingSeats".equals(action)) {
-            this.bookingSeats(request, response);
-        }
-
-    }
+  }
 
 }
--- src/java/data/DBUtil.java
+++ src/java/data/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
     private static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("CinemaPU");
+            Persistence.createEntityManagerFactory("cinemaDB");
     
     public static EntityManagerFactory getEmFactory() {
         return emf;
