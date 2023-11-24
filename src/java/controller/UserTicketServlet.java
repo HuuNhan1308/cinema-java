8dc1eba1f356053c3e78c071b51a9c93487c1036
hhnhan1308 <nhanhohuunhan7398@gmail.com>
2023-11-24 21:18:27 +0700

    add check out in booking showtime

--- src/java/controller/UserTicketServlet.java
+++ src/java/controller/UserTicketServlet.java
@@ -10,6 +10,8 @@ import data.CookieUtil;
 import data.CustomerDB;
 import data.InvoiceDB;
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,11 +21,11 @@ import javax.servlet.http.Cookie;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "UserTicketServlet", urlPatterns = {"/ticket"})
+@WebServlet(name = "UserTicketServlet", urlPatterns = { "/ticket" })
 public class UserTicketServlet extends HttpServlet {
 
   protected void show(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
+      throws ServletException, IOException {
     String url = "/ticket.jsp";
 
     // Get user from session
@@ -35,7 +37,7 @@ public class UserTicketServlet extends HttpServlet {
     synchronized (lock) {
       customer = (Customer) session.getAttribute("customer");
     }
-    
+
     // Dont have in session? check in cookie
     if (customer == null) {
       // Get all cookies
@@ -43,7 +45,7 @@ public class UserTicketServlet extends HttpServlet {
       String customerId = CookieUtil.getCookieValue(cookies, "customerId");
 
       if (customerId.isEmpty() || customerId == null) {
-        //check if customerId not exist in cookie --> redirect to login page
+        // check if customerId not exist in cookie --> redirect to login page
         response.sendRedirect(request.getContextPath() + "/login");
         return;
       } else {
@@ -52,7 +54,6 @@ public class UserTicketServlet extends HttpServlet {
       }
     }
 
-    
     String customerId = customer.getCustomerId();
     // Select all ticket with this customerId
     List<Invoice> invoices = InvoiceDB.selectInvoices(customerId);
@@ -64,17 +65,50 @@ public class UserTicketServlet extends HttpServlet {
 
   }
 
+  protected void refund(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String url = "/ticket.jsp";
+
+    Customer customer = (Customer) request.getSession().getAttribute("customer");
+    String InvoiceID = request.getParameter("InvoiceID");
+    Invoice invoice = InvoiceDB.selectInvoice(InvoiceID);
+
+    // compare with current date
+    Date showtimeDate = invoice.getTickets().get(0).getShowtime().getDate();
+
+    // Get the date two days from now
+    LocalDate twoDaysFromNow = LocalDate.now().plusDays(2);
+
+    // cannot refund if exceed 2 days before
+    if (showtimeDate.before(Date.valueOf(twoDaysFromNow))) {
+      request.setAttribute("state", "fail");
+      request.setAttribute("invoices", InvoiceDB.selectInvoices(customer.getCustomerId()));
+      request.getRequestDispatcher("/ticket.jsp").forward(request, response);
+      return;
+    }
+
+    double refundTotal = invoice.getTotalPrice();
+    InvoiceDB.delete(invoice);
+
+    customer.setBalance(customer.getBalance() + refundTotal);
+    CustomerDB.update(customer);
+
+    request.setAttribute("state", "success");
+    request.setAttribute("invoices", InvoiceDB.selectInvoices(customer.getCustomerId()));
+    request.getRequestDispatcher(url).forward(request, response);
+  }
+
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
+      throws ServletException, IOException {
 
     this.show(request, response);
   }
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-
+      throws ServletException, IOException {
+    this.refund(request, response);
   }
 
 }
